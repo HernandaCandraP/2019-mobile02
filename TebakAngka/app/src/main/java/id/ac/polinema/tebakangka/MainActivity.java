41dadeab697a743084ce07b07c835b117aa4d138
HernandaCandraP <hernandacp@gmail.com>
2019-09-04 17:29:45 +0700

    Membuat aplikasi tebak angka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,24 +13,48 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText input;
+	private Button tebak;
+	int random;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		initRandomNumber();
+		input = findViewById(R.id.number_input);
+		tebak = findViewById(R.id.guess_button);
+		Toast.makeText(this, ""+random, Toast.LENGTH_SHORT).show();
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
-
+	private int initRandomNumber() {
+		Random r = new Random();
+		random = r.nextInt(100);
+		return random;
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		int angka = Integer.parseInt(String.valueOf(input.getText()));
+		if(angka > random){
+			Toast.makeText(this, "Hasil Terlalu Besar", Toast.LENGTH_SHORT).show();
+		}
+		else if(angka < random){
+			Toast.makeText(this, "Hasil Terlalu Kecil", Toast.LENGTH_SHORT).show();
+		}
+		else {
+			Toast.makeText(this, "Hasil Sama", Toast.LENGTH_SHORT).show();
+			tebak.setEnabled(false);
+		}
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		input.setText("");
+		initRandomNumber();
+		tebak.setEnabled(true);
 	}
 }
